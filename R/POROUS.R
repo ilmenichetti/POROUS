c3b04f7152c2a8596cc72bec29013bf92c412f7d
Lorenzo <ilmenichetti@gmail.com>
2023-02-15 13:39:28 +0100

    Draft of the first version, tested only partially

---
+++ R/POROUS.R
@@ -0,0 +1,170 @@
+#' The SOC decomposition model
+#'
+#' This function implements with the SoilR model development framework the dual porosity model described in Meurer et al. (2020).
+#' The model is an evolution of a two-pool linear SOC model, with two pools (young and old material9) running in parallel for micro and mesopores.
+#' While aboveground inputs are rooted in the mesopores, root inputs are distributed between micro and mesopores depending on porosity, which is in turn influenced by organic matter. This makes the model nonlinear, although it still behaves similarly to a linear model within a reasonable calibration range. The model is described by a series of four equations: \cr
+#' \cr
+#' \eqn{\frac{dM_{Y_{(mes)}}}{dt} = I_m + \left( \frac{\phi_{mes}}{\phi_{mes}+\phi_{mic}}\right) \cdot I_r - k_Y \cdot M_{Y_{(mes)}}+ T_Y } \cr
+#' \eqn{ \frac{dM_{O_{(mes)}}}{dt} = \left( \epsilon \cdot k_Y \cdot M_{Y_{(mes)}} \right) - \left( (1- \epsilon) \cdot k_O \cdot M_{O_{(mes)}} \right) + T_O} \cr
+#' \eqn{ \frac{dM_{Y_{(mic)}}}{dt} = \left( \frac{\phi_{mic}}{\phi_{mes}+\phi_{mic}}\right) \cdot I_r - k_Y \cdot F_{prot} \cdot M_{Y_{(mes)}}- T_Y } \cr
+#' \eqn{ \frac{dM_{O_{(mic)}}}{dt} = \left( \epsilon \cdot k_Y \cdot F_{prot} \cdot M_{Y_{(mes)}} \right) - \left( (1- \epsilon) \cdot k_O \cdot F_{prot} \cdot M_{O_{(mes)}} \right) - T_O } \cr
+#'  \cr
+#' Please refer to the original paper for more details.  \cr
+#'  \cr
+#' The two porosity terms, \eqn{\phi_{mes} = f(M_{Y_{(mes)}}, M_{O_{(mes)}},M_{Y_{(mic)}}, M_{O_{(mic)}})} and \eqn{\phi_{mic} = f(M_{Y_{(mic)}}, M_{O_{(mic)}})}, are dependent on the variation of the different C pools and everything is variable over time, introducing a nonlinearity in the system and defining the biggest peculiarity of this model.  \cr
+#' After substitutung the terms \eqn{\left( \frac{\phi_{mes}(t)}{\phi_{mes}(t)+\phi_{mic}(t)}\right) = \varphi_{mes}} and \eqn{\left( \frac{\phi_{mic}(t)}{\phi_{mes}(t)+\phi_{mic}(t)}\right) = \varphi_{mic}},
+#' The model can be rewritten in matrix form as :  \cr
+#'  \cr
+#' \eqn{I_m(t) + I_r(t) \cdot N(C,t) + A(t) \cdot P(t) \cdot C(t)} \cr
+#' Or, more explicitly:  \cr
+#'  \cr
+#'  \eqn{\frac{dC}{dt}=\begin{bmatrix} I_m\\ 0\\ 0\\ 0\\ \end{bmatrix}+   \begin{bmatrix} I_r\\ 0\\ I_r\\ 0\\ \end{bmatrix} \cdot \begin{bmatrix} \varphi_{mes} & 0 & 0 & 0\\ 0 & 1 & 0 & 0\\ 0 & 0 & \varphi_{mic} & 0\\ 0 & 0 & 0 & 1\\ \end{bmatrix}+   \begin{bmatrix} -k_y & \epsilon & 0 & 0\\ 0 & -k_o & 0 & 0\\ T_Y & 0 & -k_y & \epsilon\\ 0 & T_O & 0 & -k_o\\ \end{bmatrix} \cdot \begin{bmatrix} 1 & 0 & 0 & 0\\ 0 & 1 & 0 & 0\\ 0 & 0 & F_{prot} & 0\\ 0 & 0 & 0 & F_{prot}\\ \end{bmatrix} \cdot \begin{bmatrix} M_{Y_{mes}}\\ M_{O_{mes}}\\ M_{Y_{mic}}\\ M_{O_{mic}} \end{bmatrix} }
+#'
+#' @param ky decomposition constant of the Young pool
+#' @param ko decomposition constant of the Old pool
+#' @param kmix mixing rate
+#' @param e efficiency, which is the transfer term between the pools and corresponds to the term h in the ICBM model in Kätterer et al. (2001)
+#' @param Im Inputs from aboveground
+#' @param Ir Inputs from roots
+#' @param F_prot protection provided by the micropore space
+#' @inheritParams pore_frac
+#' @inheritParams Delta_z
+#' @inheritParams f_text_mic
+#' @return two values, the proportion of input in the mesopore and micropore Y pools
+#'
+#' @references Meurer, Katharina Hildegard Elisabeth, Claire Chenu, Elsa Coucheney, Anke Marianne Herrmann, Thomas Keller, Thomas Kätterer, David Nimblad Svensson, and Nicholas Jarvis. “Modelling Dynamic Interactions between Soil Structure and the Storage and Turnover of Soil Organic Matter.” Biogeosciences 17, no. 20 (October 19, 2020): 5025–42. https://doi.org/10.5194/bg-17-5025-2020. \cr
+#' Kätterer, Thomas, and Olof Andrén. “The ICBM Family of Analytically Solved Models of Soil Carbon, Nitrogen and Microbial Biomass Dynamics — Descriptions and Application Examples.” Ecological Modelling 136, no. 2–3 (January 2001): 191–207. https://doi.org/10.1016/S0304-3800(00)00420-8.
+#' @export
+#'
+Porous<-function(ky=0.8, ko=0.00605,
+                 kmix=0.9,
+                 e=0.13,
+                 Im=1.1, Ir=0.5,
+                 F_prot=0.0,
+                 phi_mac=0.2,
+                 clay=0.2,
+                 Delta_z_min=20,
+                 gamma_o=1.2){
+
+  time_symbol='t'
+
+
+
+  ##### IN
+  ifs=SoilR:::InFluxList_by_PoolName(
+    c(
+      SoilR:::InFlux_by_PoolName(
+        destinationName='My_mes',
+        func=function(t){
+          Im
+        }
+      ),
+      SoilR:::InFlux_by_PoolName(
+        destinationName='My_mes',
+        func=function(t){
+          Ir*pore_frac(phi_mac, clay, Delta_z_min, gamma_o)[1]
+        }
+      ),
+      SoilR:::InFlux_by_PoolName(
+        destinationName='My_mic',
+        func=function(t){
+          Ir*pore_frac(phi_mac, clay, Delta_z_min, gamma_o)[2]
+        }
+      )
+    )
+  )
+  ##### OUT
+  ofs=SoilR:::OutFluxList_by_PoolName(
+    c(
+      SoilR:::OutFlux_by_PoolName(
+        sourceName='My_mes',
+        func=function(My_mes){
+          ky*My_mes
+        }
+      )
+      ,
+      SoilR:::OutFlux_by_PoolName(
+        sourceName='Mo_mes',
+        func=function(Mo_mes){
+          (1-e)*ko*Mo_mes
+        }
+      ),
+      SoilR:::OutFlux_by_PoolName(
+        sourceName='My_mic',
+        func=function(My_mic){
+          ky*F_prot*My_mic
+        }
+      ),
+      SoilR:::OutFlux_by_PoolName(
+        sourceName='Mo_mic',
+        func=function(Mo_mic){
+          (1-e)*ko*F_prot*Mo_mic
+        }
+      ),
+      # Bioturbation, nonlinearity here, Ty and To are outfluxes from the micropores (and also internal fluxes below, into My_mes and Mo_mes)
+      #Ty
+      SoilR:::OutFlux_by_PoolName(
+        sourceName='My_mic',
+        func=function(My_mic,My_mes){
+          kmix*((My_mic-My_mes)/2)
+        }
+      ),
+      #To
+      SoilR:::OutFlux_by_PoolName(
+        sourceName='Mo_mic',
+        func=function(Mo_mic,Mo_mes){
+          kmix*((Mo_mic-Mo_mes)/2)
+        }
+      )
+    )
+  )
+
+  ##### INT
+  intfs=SoilR:::InternalFluxList_by_PoolName(
+    list(
+      SoilR:::InternalFlux_by_PoolName(
+        sourceName='My_mes',
+        destinationName='Mo_mes',
+        func=function(My_mes){
+          e*ky*My_mes
+        }
+      ),
+      SoilR:::InternalFlux_by_PoolName(
+        sourceName='My_mic',
+        destinationName='Mo_mic',
+        func=function(My_mic){
+          e*ky*F_prot*My_mic
+        }
+      ),
+      #bioturbation fluxes
+      #Ty
+      SoilR:::InternalFlux_by_PoolName(
+        sourceName='My_mic',
+        destinationName='My_mes',
+        func=function(My_mic, My_mes){
+          kmix*((My_mic-My_mes)/2)
+        }
+      ),
+      #To
+      SoilR:::InternalFlux_by_PoolName(
+        sourceName='Mo_mic',
+        destinationName='Mo_mes',
+        func=function(Mo_mic, Mo_mes){
+          kmix*((Mo_mic-Mo_mes)/2)
+        }
+      )
+    )
+  )
+
+  smod <- SoilR:::SymbolicModel_by_PoolNames(
+    in_fluxes=ifs,
+    internal_fluxes=intfs,
+    out_fluxes=ofs,
+    timeSymbol=time_symbol
+  )
+  smod
+}
+
+
+
+
---
+++ R/modifiers.R
@@ -0,0 +1,138 @@
+#' The main accessory function of the model
+#'
+#' This function  calculates the proportion of inputs in each of the two youg pools deending on the organic matter content
+#' @param phi_mac macroporosity
+#' @param Delta_z_min minimal soil thickness if no organic matter was present
+#' @param clay fraction of clay content
+#' @param gamma_o density of organic matter
+#' @param My_mic One of the four model pools (they are all summed up in this function for calculating the total)
+#' @param Mo_mic One of the four model pools (they are all summed up in this function for calculating the total)
+#' @param My_mes One of the four model pools (they are all summed up in this function for calculating the total)
+#' @param Mo_mes One of the four model pools (they are all summed up in this function for calculating the total)
+#' @return two values, the proportion of input in the mesopore and micropore Y pools
+#' @export
+
+pore_frac<-function(phi_mac=0.2,
+                    clay=0.2,
+                    Delta_z_min=20,
+                    gamma_o=1.2,
+                    My_mic,
+                    Mo_mic,
+                    My_mes,
+                    Mo_mes){
+
+    phi_mic_calc<-phi_mic(My_mic=1, Mo_mic=2, My_mes=1, Mo_mes=10, gamma_o=gamma_o, clay=clay, Delta_z_min = Delta_z_min, phi_mac=phi_mac)
+
+    phi_mat_calc<-phi_mat(phi_mic=phi_mic_calc, My_mic=1, Mo_mic=2, My_mes=1, Mo_mes=10, gamma_o=gamma_o, Delta_z_min = Delta_z_min, phi_mac=phi_mac)
+
+    phi_mes_calc=phi_mat_calc-phi_mic_calc
+
+  mes_f=phi_mes_calc/(phi_mes_calc+phi_mic_calc)
+  mic_f=phi_mic_calc/(phi_mes_calc+phi_mic_calc)
+  return(c(mes_f, mic_f))
+}
+
+
+#' Proportion of micropores
+#'
+#' This function calculates the proportion of the textural pore space that comprises micropores. It is used in \code{\link{pore_frac}}
+#' @param clay soil clay fraction
+#' @param phi_min minimal porosity, user defined
+#' @return one single value
+#' @export
+
+f_text_mic<-function(clay, phi_min){
+
+  psi_w=-150 #meters, the wilting point pressure head
+  theta_w=0.004+0.5*clay #psi_w (m) is the wilting point pressure head (= -150 m) and the corresponding water content theta_w (m3 m-3) is estimated from a pedotransfer function (Ostovari et al., 2015)
+  lamda_mat=log(theta_w/phi_min)/log(-0.3/psi_w)
+
+  f_text_mic=(-0.3/-6)^lamda_mat
+  return(f_text_mic)
+}
+
+
+#' Variation of the thickness of soil layer
+#'
+#' This function calculates the variation of the thickness of soil layer as a function of organic matter
+#' @param f_agg an aggregation factor (m3 pore space m−3 organic matter) defined as the slope of the linear relationship assumed between the volume of aggregation pore space \eqn{V_{agg}}, and the volume of organic matter \eqn{V_{s_o}}
+#' @param Delta_z_min minimal soil thickness if no organic matter was present
+#' @inheritParams pore_frac
+#' @param gamma_o density of organic matter
+#' @return one single value
+#' @export
+
+Delta_z<-function(f_agg=3,
+                  Delta_z_min,
+                  My_mic, Mo_mic, My_mes, Mo_mes,
+                  phi_mac,
+                  gamma_o){
+
+  Mso=My_mic + Mo_mic + My_mes + Mo_mes
+  Delta_z=(((1+f_agg)*(Mso/gamma_o))+Delta_z_min)/(1-phi_mac)
+
+  return(Delta_z)
+}
+
+
+
+#' Microporosity
+#'
+#' This function calculates the microporosity \eqn{\phi_{mic}} based on the variation of organic matter in the soil. It is used in \code{\link{pore_frac}}
+#' @inheritParams pore_frac
+#' @inheritParams Delta_z
+#' @inheritParams f_text_mic
+#' @return one single value
+#' @export
+
+phi_mic<-function(My_mic, Mo_mic, My_mes, Mo_mes,
+                  gamma_o, #density of organic matter
+                  f_agg=3, #default suggested by Nick Jarvis
+                  clay,
+                  Delta_z_min, #soil layer thickess (m)
+                  phi_min=1, #minimum matrix porosity, STILL MISSING
+                  phi_mac){
+
+  f_text_mic_calc=f_text_mic(clay=clay, phi_min=phi_min)
+
+  Delta_z_calc=Delta_z(f_agg=3,
+                       Delta_z_min=Delta_z_min,
+                       My_mic, Mo_mic, My_mes, Mo_mes,
+                       phi_mac,
+                       gamma_o)
+
+  phi_mic=((f_agg*((My_mic+Mo_mic)/gamma_o))+f_text_mic_calc*Delta_z_min*phi_min)/Delta_z_calc
+  return(phi_mic)
+}
+
+
+
+
+#' Matrix porosity
+#'
+#' This function calculates the matrix porosity \eqn{\phi_{mac}} based on the variation of organic matter in the soil. It is used in \code{\link{pore_frac}} to calculate mesoporosity \eqn{\phi_{mes}=\phi_{mat}-\phi_{mic}}
+#' @inheritParams pore_frac
+#' @inheritParams Delta_z
+#' @inheritParams f_text_mic
+#' @return one single value
+#' @export
+phi_mat<-function(My_mic, Mo_mic, My_mes, Mo_mes,
+                  gamma_o,
+                  f_agg=3,
+                  Delta_z_min,#soil layer thickess (m)
+                  phi_min=1,
+                  phi_mac#minimum matrix porosity, STILL MISSING
+                  ){
+
+  Mso=My_mic + Mo_mic + My_mes + Mo_mes
+
+  Delta_z_calc=Delta_z(f_agg=3,
+                       Delta_z_min = Delta_z_min,
+                       My_mic, Mo_mic, My_mes, Mo_mes,
+                       phi_mac,
+                       gamma_o)
+
+
+  phi_mat=(f_agg*(Mso/gamma_o)+Delta_z_min*phi_min)/Delta_z_calc
+  return(phi_mat)
+}
